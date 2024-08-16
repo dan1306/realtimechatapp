10b6c52cca85f553184f6214eb17d65d41206e58
dan1306 <81488150+dan1306@users.noreply.github.com>
2024-08-15 22:12:56 -0400

    creating navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import "../styles/navbar.css"
+ 
+const NavBar = () => {
+    return (
+        <div className="navbar-basic-styling">
+            <div className="navbar-elements">
+                <div className="nav-logo">logo</div>
+                <div className="nav-user-dropdown-and-settings">
+                    <div className="nav-user-avatar">
+
+                    </div>
+                    <div className="nav-user-dropdown">
+                        asda
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default NavBar;

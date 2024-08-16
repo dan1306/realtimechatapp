865a542a3dd816fa1ef0eb3fce1c4a67f1a58a81
dan1306 <81488150+dan1306@users.noreply.github.com>
2024-08-16 11:43:51 -0400

    navbar done for the most part

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,55 @@
+"use client"
+
+import { useState } from "react";
 import "../styles/navbar.css"
+import { MessagesSquare } from 'lucide-react';
+
  
 const NavBar = () => {
+
+    const [dropUserOption, SetdropUserOption] = useState<boolean>(false)
+
     return (
         <div className="navbar-basic-styling">
             <div className="navbar-elements">
-                <div className="nav-logo">logo</div>
+                <div className="nav-logo">
+                    <MessagesSquare className="nav-messages-square" />
+                    <p className="nav-titile">
+                        ChatMeUp
+                    </p>
+                </div>
+                {/* <div>
+                    adas
+                </div> */}
                 <div className="nav-user-dropdown-and-settings">
-                    <div className="nav-user-avatar">
+                    <div className="nav-user-avatar" onClick={()=> SetdropUserOption(!dropUserOption)}>
 
                     </div>
-                    <div className="nav-user-dropdown">
-                        asda
+                    <div className={`nav-user-dropdown ${dropUserOption ? '' : 'hide-user-dropdown'}`}>
+                    <div className="nav-user-name-and-email">
+                            <p className="username">
+                                username
+                            </p>
+                            <p className="email">
+                                email
+                            </p>
+                        </div>
+                        <hr className="line" />
+                        <div>
+                            <p className="nav-feed">
+                                Feed
+                            </p>
+                            <p className="nav-create-a-chat">
+                                Create A Chat
+                            </p>
+                            <p className="nav-settings">
+                                Settings
+                            </p>
+                        </div>
+                        <hr className="line" />
+                        <div className="signIn-signOut">
+                            SignIn / Sign Out
+                        </div>
                     </div>
                 </div>
             </div>
